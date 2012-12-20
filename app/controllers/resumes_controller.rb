a2bc235a463581de6e8bb9074a86134794eeecf1
kerrieyee <kerrie@kerrieyee.com>
2012-12-19 17:06:34 -0800

    started resume app and started devise process and resume controller and model

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,25 @@
+class ResumesController < ApplicationController
+  def index
+    @resumes = Resume.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resumeapp::Application.config.secret_token = 'cfb0d198f299fae868c0e521513bd0c1d2bf3fc504235594e2fa21397a0c32bd98927d9c269c08cc61a249f75041c7f3da1a280fa9ab0bb37d5cc89b44c5498b'
