9576483f33a5873a11757dac8e44b93665a806dd
kerrieyee <kerrie@kerrieyee.com>
2012-12-24 18:25:44 -0800

    updated user feature specs

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "Users", :js => true do
   include Warden::Test::Helpers
-
+  let!(:user){Fabricate(:user)}
+  let!(:user2){Fabricate(:user)}
   context "when user is not logged in" do
-  	let!(:user){Fabricate(:user)}
   	describe "users#index" do 
   		it "should redirect to the login page if trying to access the resumes page" do
   			visit resumes_path
@@ -12,9 +12,16 @@ describe "Users", :js => true do
   		end
 
   		it "should not have access to other people's resumes" do
-  			visit users_path(user)
+  			visit users_path(user2)
   			page.should have_content "You need to sign in or sign up before continuing."
   		end
   	end
   end
+
+  # context "when user is logged in" do
+  # 	before(:each) do
+  # 		login_as user, :scope => user
+  # 	end
+  # end
+
 end
