5231925262eb813edad41562c0693d630ac023b4
kerrieyee <kerrie@kerrieyee.com>
2013-01-09 19:44:43 -0800

    added has and belongs to many relationship to resume model

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,4 +3,5 @@ class Resume < ActiveRecord::Base
   validates :name, :presence => true
   validates :user, :presence => true
   belongs_to :user
+  has_and_belongs_to_many :job_descriptions, :join_table => :job_descriptions_resumes
 end
