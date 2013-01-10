321cafa85d033118743722a25a35971970dd3d5c
kerrieyee <kerrie@kerrieyee.com>
2013-01-09 19:40:24 -0800

    updated resume spec to reflect has and belongs to many relationship with job description

--- spec/models/resume_spec.rb
+++ spec/models/resume_spec.rb
@@ -5,4 +5,5 @@ describe Resume do
   it {should validate_presence_of :name}
   it {should validate_presence_of :user}
   it {should belong_to :user}
+  it {should have_and_belong_to_many :job_descriptions}
 end
